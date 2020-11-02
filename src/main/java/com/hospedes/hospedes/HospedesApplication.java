472c67bb2683bb1be3eb4799e0e59026df4dda90
Adriano Zorzi <adizorzi@gmail.com>
2020-11-02 20:11:56 -0300

    Commit inicial

---
+++ src/main/java/com/hospedes/hospedes/HospedesApplication.java
@@ -0,0 +1,13 @@
+package com.hospedes.hospedes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospedesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospedesApplication.class, args);
+	}
+
+}
